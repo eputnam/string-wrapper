d4da44176493a740f71f1dc719a8833174ccd1f3
Maggie Dreyer <maggie@puppet.com>
2016-12-14 09:42:33 -0800

    Add smarter exclusions for strings we don't want to mark

--- string_wrapper.rb
+++ string_wrapper.rb
@@ -5,13 +5,16 @@ class StringWrapper < Parser::Rewriter
   end
 
   def on_str(node)
-    puts node.loc
     if node.loc.respond_to?(:heredoc_body)
       insert_before(node.loc.expression, "_(")
       insert_after(node.loc.expression, ")")
-    elsif node.loc.respond_to?(:begin) # avoid constants, like __FILE__, which should not be marked
-      # do not mark empty strings, which are just two quotes
-      return if node.loc.expression.length == 2
+    elsif node.loc.respond_to?(:begin)
+      # avoid constants, like __FILE__, which do not have begin/end nodes,
+      # and should not be marked
+      str = node.children[0]
+      return if str == ""         # ignore empty strings
+      return if str !~ /[A-Za-z]/ # ignore non-text strings
+      return if str =~ /^\.*\//   # ignore many file paths
 
       insert_before(node.loc.begin, "_(")
       insert_after(node.loc.end, ")")
@@ -20,9 +23,9 @@ class StringWrapper < Parser::Rewriter
 
   def on_send(node)
     method_name = node.loc.selector.source
-    if ((method_name == 'require') ||
-      (method_name == "_") ||
-      (/\[.*\]/.match method_name))
+    if ((method_name == 'require') || # ignore requires
+      (method_name == "_") ||         # ignore marked strings
+      (/\[.*\]/.match method_name))   # ignore hash keys
       return
     end
     super
